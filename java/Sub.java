d837a003118d2675c9eff6ec0c12141b083f3bec
MilkyWay <112637061+mkway999@users.noreply.github.com>
2023-11-12 20:15:31 +0900

    [시스템] 부자재 객체 클래스

--- java/Sub.java
+++ java/Sub.java
@@ -1,12 +1,11 @@
 import java.util.HashMap;
 import java.util.Map;
-
 class Sub
 {
 	public static Map<Integer, SubProducts> sub = new HashMap<Integer, SubProducts>();
 	public static void set()
 	{
-		sub.put(1,new SubProducts("¼³ÅÁ",0,10500));
-		sub.put(2,new SubProducts("²¿Ä¡",0,350));
+		sub.put(1,new SubProducts("Â¼Â³Ã…Ã",0,10500));
+		sub.put(2,new SubProducts("Â²Â¿Ã„Â¡",0,350));
 	}
 }
